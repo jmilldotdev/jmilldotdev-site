d2a3fc4cccafa4bc04ce5f525bb97d92fc3c55e5
Jonathan Miller <jon@jmill.dev>
2021-09-05 16:54:13 -0400

    fix

--- components/StartHere.tsx
+++ components/StartHere.tsx
@@ -1,12 +1,13 @@
-import { AvatarIcon } from '@icons/AvatarIcon'
 import Link from 'next/link'
 
 export const StartHere = () => {
+  const titleContent = `Hi, I'm Jonathan`
   return (
     <div>
-      <h2>Hi, I'm Jonathan</h2>
+      <h2>{titleContent}</h2>
 
-      <p>I am a software developer and writer based in Richmond, Virginia. I am interested in creative programming, generative art, and human-machine synergy.</p>
+      <p>I am a software developer and writer based in Richmond, Virginia. 
+        I am interested in creative programming, generative art, and human-machine synergy.</p>
 
       <p>Want to learn more?</p>
 
