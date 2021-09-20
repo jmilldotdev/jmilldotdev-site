9f41913f0c7c2e352c862a452eda02bf94ffc98d
Jonathan Miller <jon@jmill.dev>
2021-09-20 10:53:29 -0400

    Remove production check

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,25 +20,23 @@ export default class MyDocument extends Document {
       <Html {...{ lang, className: 'casper' }}>
         <Head>
           <link rel="alternate" type="application/rss+xml" title="Jamify RSS Feed" href={`${resolve(processEnv.siteUrl, 'rss.xml')}`} />
-          {isProduction && (
-            <Fragment>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
+          <Fragment>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-                  gtag('config', '${GA_MEASUREMENT_ID}', {
-                    page_path: window.location.pathname,
-                  })
-                  `,
-                }}
-              />
-            </Fragment>
-          )}
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                })
+                `,
+              }}
+            />
+          </Fragment>
         </Head>
         <body {...{ className: bodyClass }}>
           <script
