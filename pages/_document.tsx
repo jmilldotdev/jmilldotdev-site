a02ae4d59a5af3bab9d75e3fe3c5d744d6ec5793
Jonathan Miller <jon@jmill.dev>
2021-09-20 10:40:51 -0400

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,46 @@
-
+import { Fragment } from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 import { resolve } from 'url'
+import { GA_MEASUREMENT_ID } from '@lib/gtag'
 import { processEnv } from '@lib/processEnv'
 
 export default class MyDocument extends Document {
-
   static async getInitialProps(ctx: DocumentContext) {
     return await super.getInitialProps(ctx)
   }
 
   render() {
+    const isProduction = process.env.NODE_ENV === 'production'
     const { pageProps } = this.props.__NEXT_DATA__.props
-    const { cmsData, settings }  = pageProps || { cmsData: null, settings: null }
-    const { settings: cmsSettings , bodyClass } = cmsData || { settings: null, bodyClass: '' }
+    const { cmsData, settings } = pageProps || { cmsData: null, settings: null }
+    const { settings: cmsSettings, bodyClass } = cmsData || { settings: null, bodyClass: '' }
     const { lang } = settings || cmsSettings || { lang: 'en' }
 
     return (
-      <Html {...{lang, className: 'casper' }}>
+      <Html {...{ lang, className: 'casper' }}>
         <Head>
-          <link
-            rel="alternate"
-            type="application/rss+xml"
-            title="Jamify RSS Feed"
-            href={`${resolve(processEnv.siteUrl, 'rss.xml')}`}
-          />
+          <link rel="alternate" type="application/rss+xml" title="Jamify RSS Feed" href={`${resolve(processEnv.siteUrl, 'rss.xml')}`} />
+          {isProduction && (
+            <Fragment>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  })
+                  `,
+                }}
+              />
+            </Fragment>
+          )}
         </Head>
-        <body {...{className: bodyClass}}>
+        <body {...{ className: bodyClass }}>
           <script
             dangerouslySetInnerHTML={{
               __html: `
